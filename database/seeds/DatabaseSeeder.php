d5b82eba66eb933c25288cec211b0584c0dadcf5
Alessandro <alecritelli@gmail.com>
2020-06-25 19:47:02 +0200

    1 to many mods.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(EmployeeSeeder::class);
+         $this->call([
+
+           CategoriesSeeder::class,
+           PostsSeeder::class
+         ]);
     }
 }
---
+++ database/seeds/PostsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+use App\Category;
+use App\Post;
+use Illuminate\Database\Seeder;
+
+class PostsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
